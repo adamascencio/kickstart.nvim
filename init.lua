73fb13a6035208ae1d7d090be87d621239d3b3e4
adamascencio <ascencioadam@gmail.com>
2025-04-27 14:33:55 -0700

    chore: disable linter plugin and install markdown LSP

--- init.lua
+++ init.lua
@@ -680,6 +680,7 @@ require('lazy').setup({
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         ts_ls = {},
+        marksman = {},
         --
 
         lua_ls = {
@@ -992,7 +993,7 @@ require('lazy').setup({
   --
   require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
-  require 'kickstart.plugins.lint',
+  -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
