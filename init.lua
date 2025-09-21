8b4e376d79dea67d51d887378b7564b77e8835a4
adamascencio <ascencioadam@gmail.com>
2025-09-21 09:10:05 -0700

    LSP: Install black python formatter

--- init.lua
+++ init.lua
@@ -677,12 +677,22 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
+
+        -- bash
         bashls = {},
+        -- python
         pyright = {},
         isort = {},
+        black = {},
+
+        -- html/css
         html = {},
         cssls = {},
+
+        -- sql
         postgres_lsp = {},
+
+        -- markdown
         markdownlint = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
